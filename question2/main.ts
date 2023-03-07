18bef5a21e9a1b17941373bc6e84f6e6567f97b2
Felicity Brammer <felicitybrammer@gmail.com>
2023-03-07 10:40:36 -0800

    trying question 2 with a set to find unique value

--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,17 @@
 export function findOutlier(integers: number[]): number {
-    
+  // to search for an outlier, I want a solution with the least amount of time complexity possible
+  // avoid using nested loops, since integers array can be quite large
+  // a set can only store unique values so try converting to set
+  let nArray = [...new Set<number>(integers)];
+
+  //return Array.from(arrToSet); //problem: this returns an array but we want a number
+  let N = Number(nArray.join(''));
+  return N;
+
+
   }
+  findOutlier([0, 1, 2]); //1
+  findOutlier([1, 2, 3]);//2
+  findOutlier([2, 6, 8, 10, 3]);//3
+  findOutlier([0, 0, 3, 0, 0])//3
+  findOutlier([1, 1, 0, 1, 1])//0
