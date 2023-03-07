2f0208e716ce7744fcc30883e6b4e8cf22ce4e99
Felicity Brammer <felicitybrammer@gmail.com>
2023-03-07 09:21:16 -0800

    tests in file passing

--- question1/main.js
+++ question1/main.js
@@ -1,17 +1,36 @@
 //export 
-
 function isValidWalk(walk) {
     
   //initial check to see if the walk will take exactly ten minutes
     if(walk.length !== 10) {
-      console.log(walk.length);
+      //console.log(walk.length);
+      console.log('false');
     return false;
   } else {
  // in order to return to the start position, the number of 'n' and 's' elements
  // must be equal, same for 'w' and 'e'
  // create an object that counts the number of each 'n' and 's' element
-  const vertCounts = walk.reduce((count, elem) => (count[elem] = count[elem] + 1 || 1, count), {});
-  console.log(vertCounts);
+  // const counts = walk.reduce((count, elem) => (count[elem] = count[elem] + 1 || 1, count), {});
+  // console.log(counts);
+  
+  
+
+  const counts = {};
+
+  walk.forEach(el => {
+    counts[el] = (counts[el] || 0) + 1;
+  });
+
+  //console.log(counts);
+
+  if(counts.n !== counts.s || counts.w !== counts.e) {
+    console.log('fales');
+    return false;
+  }  else {
+
+  console.log('true');
+  return true;
+  }
   }
   
 }
@@ -20,3 +39,4 @@ isValidWalk(['n','s','n','s','n','s','n','s','n','s']); //true
 isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']) //false
 isValidWalk(['w']) //false
 isValidWalk(['n','n','n','s','n','s','n','s','n','s']) //false
+isValidWalk(['n', 's', 'w', 'e', 'n', 's', 'w', 'e', 'w', 'e']); //true
