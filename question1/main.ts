57bcbedf828ad21192b4369619c22401cf4f2aa3
Felicity Brammer <felicitybrammer@gmail.com>
2023-03-09 11:12:08 -0800

    tidy up comments

--- question1/main.ts
+++ question1/main.ts
@@ -2,29 +2,23 @@ export function isValidWalk(walk: string[]) {
   //initial check to see if the walk will take exactly ten minutes
   if (walk.length !== 10) {
     //console.log(walk.length);
-    //console.log("false");
     return false;
   } else {
     // in order to return to the start position, the number of 'n' and 's' elements
     // must be equal, same for 'w' and 'e'
     // create an object that counts the number of each element
     // const counts = walk.reduce((count, elem) => (count[elem] = count[elem] + 1 || 1, count), {});
-    // console.log(counts);
 
     const counts: any = {};
 
     walk.forEach((el) => {
       counts[el] = (counts[el] || 0) + 1;
     });
-
-    //console.log(counts);
     
     // check for lack of equal numbers of 'n' and 's' or 'w' and 'e'
     if (counts.n !== counts.s || counts.w !== counts.e) {
-      //console.log("false");
       return false;
     } else {
-      //console.log("true");
       return true;
     }
   }
